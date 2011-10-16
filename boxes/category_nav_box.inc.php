6f87fc506d0f230f4a9b91cca2271b90f03ec7c9
Aare Laanesaar <woozy014@hotmail.com>
2011-10-16 09:52:21 +0100

    pushing code logic more into the controller

--- boxes/category_nav_box.inc.php
+++ boxes/category_nav_box.inc.php
@@ -20,37 +20,9 @@ if($categories->data) {
 
 $nav_box = new XTemplate ('boxes'.CC_DS.'category_nav_box.tpl');
 
-$first = $categories->get_categories('cat_father_id = 0'); 
+$navigation = $categories->navigation;
 
-$output .= '<ul id="nav">';
-
-for($i=0; $i<=count($first)-1; $i++) { 
-
-$second = $categories->get_categories('cat_father_id =' . $first[$i]['cat_id']); 
-
-$output .= "<li>" . "<a>" . $first[$i]['cat_name'] . "</a>"; 
-
-for($j=0; $j<=count($second)-1; $j++) { 
-
-$output .= "<ul><li>" . "<a>" . $second[$j]['cat_name'] . "</a>"; 
-
-$third = $categories->get_categories('cat_father_id = ' . $second[$j]['cat_id']);
-
-for($k=0; $k<=count($third)-1; $k++) {
-
-$output .= "<ul><li>" . "<a>" . $third[$k]['cat_name'] . "</a>" . "</li></ul>"; } 
-
-$output .= "</li></ul>";
-
- } 
-
-$output .= "</li>";
-
- } 
-
-$output .= "</ul>";
-
-$nav_box->assign('CATEGORY_NAVIGATION', $output);
+$nav_box->assign('CATEGORY_NAVIGATION', $navigation);
 		
 $nav_box->parse('categories');
 
@@ -59,5 +31,3 @@ $nav_box = $nav_box->text('categories');
 }
 
 ?>
-
-<ul>
--- classes/index.php
+++ classes/index.php
@@ -2,13 +2,13 @@
 
 $categories = $cat->get_categories('cat_father_id = 0');
 
-$output = "<ul>";
+$output = '<ul id="nav">';
 
 for($i=0; $i<=count($categories)-1; $i++) { 
 
 $second = $cat->get_categories('cat_father_id = ' . $categories[$i]['cat_id']); 
 
-$output .= "<li>" . $categories[$i]['cat_name'];
+$output .= "<li><a href='#'>" . $categories[$i]['cat_name']."</a>";
 
 if(isset($second[$i]['cat_id'])) {
 
@@ -16,7 +16,7 @@ $output .= "<ul>";
 
 for($j=0; $j<=count($second)-1; $j++) { 
 
-$output .= "<li>" . $second[$j]['cat_name'];
+$output .= "<li><a href='#'>" . $second[$j]['cat_name']."</a>";
 
 $third = $cat->get_categories('cat_father_id = ' . $second[$j]['cat_id']);
 
@@ -26,7 +26,7 @@ $output .= "<ul>";
 
 for($k=0; $k<=count($third)-1; $k++) {
 
-$output .= "<li>" . $third[$k]['cat_name'] . "</li>";
+$output .= "<li><a href='#'>" . $third[$k]['cat_name'] . "</a></li>";
 
 if($k == count($third)-1) { $output .= "</ul>"; }
 
@@ -56,7 +56,27 @@ $output .= "</li>";
 
 $output .= "</ul>";
 
-echo $output;
-
 ?>
 
+<html>
+<head>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+ 	<!--[if lt IE 8]>
+		<script src ="http://ie7-js.googlecode.com/svn/version/2.1(beta2)/IE8.js"></script>
+	<![endif]-->	
+</head>
+	<title>Navigation</title>
+</head>
+<body>
+
+<script src="http://jquery-ui.googlecode.com/svn/tags/latest/ui/jquery.effects.core.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="js/scripts.js"></script>
+
+<?php echo $output; ?>
+	
+</body>
+</html>
